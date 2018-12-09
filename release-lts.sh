e578b08fd8880c8e98ba0fe10add0326ea37a66a
Michael Wizner <mwz@users.noreply.github.com>
2018-12-09 01:47:03 +0000

    Update release scripts.

--- release-lts.sh
+++ release-lts.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 set -eu
 
-export VERSION=3.2.1
+export VERSION=2.10.0
 
 # Build
-docker build -t mwizner/sonarqube-scala-plugins:$VERSION -t mwizner/sonarqube-scala-plugins:latest $VERSION
+docker build -t mwizner/sonarqube-scala-plugins:$VERSION $VERSION
 docker build -t mwizner/sonarqube-scala-plugins:$VERSION-full $VERSION-full
 
 # Push
 docker push mwizner/sonarqube-scala-plugins:$VERSION
 docker push mwizner/sonarqube-scala-plugins:$VERSION-full
-docker push mwizner/sonarqube-scala-plugins:latest
