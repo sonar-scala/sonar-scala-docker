75bfcfdf73cdda29712401f937cbafab871ee541
Michael Wizner <mwz@users.noreply.github.com>
2018-12-31 18:21:57 +0000

    Improve generation of readme files.

--- vars.js
+++ vars.js
@@ -1,4 +1,127 @@
 {
-  "version": "3.3.0",
-  "ltsVersion": "2.10.0"
+  "current": {
+    "version": "3.3.0",
+    "sonar": "7.4",
+    "sonarDocs": "https://docs.sonarqube.org/7.4",
+    "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14549",
+    "sonarScala": "7.3.0"
+  },
+  "lts": {
+    "version": "2.10.0",
+    "sonar": "6.7.6 LTS",
+    "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+    "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=13972",
+    "sonarScala": "6.7.0"
+  },
+  "versions": {
+    "current": [
+      {
+        "version": "3.2.1",
+        "sonar": "7.4",
+        "sonarDocs": "https://docs.sonarqube.org/7.4",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14549",
+        "sonarScala": "7.2.0"
+      },
+      {
+        "version": "3.2.0",
+        "sonar": "7.4",
+        "sonarDocs": "https://docs.sonarqube.org/7.4",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14549",
+        "sonarScala": "7.2.0"
+      },
+      {
+        "version": "3.1.0",
+        "sonar": "7.4",
+        "sonarDocs": "https://docs.sonarqube.org/7.4",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14549",
+        "sonarScala": "7.1.0"
+      },
+      {
+        "version": "3.0.0",
+        "sonar": "7.3",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE73/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14464",
+        "sonarScala": "7.0.0"
+      }
+    ],
+    "lts": [
+      {
+        "version": "2.9.0",
+        "sonar": "6.7.5 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14467",
+        "sonarScala": "6.6.0"
+      },
+      {
+        "version": "2.8.0",
+        "sonar": "6.7.5 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14467",
+        "sonarScala": "6.5.1"
+      },
+      {
+        "version": "2.7.0",
+        "sonar": "6.7.4 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14377",
+        "sonarScala": "6.5.0"
+      },
+      {
+        "version": "2.6.0",
+        "sonar": "6.7.4 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14377",
+        "sonarScala": "6.4.0",
+        "sonarScalaExtra": "1.3.0"
+      },
+      {
+        "version": "2.5.0",
+        "sonar": "6.7.3 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14264",
+        "sonarScala": "6.4.0",
+        "sonarScalaExtra": "1.3.0"
+      },
+      {
+        "version": "2.4.0",
+        "sonar": "6.7.3 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14264",
+        "sonarScala": "6.3.0",
+        "sonarScalaExtra": "1.3.0"
+      },
+      {
+        "version": "2.3.0",
+        "sonar": "6.7.3 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14264",
+        "sonarScala": "6.2.0",
+        "sonarScalaExtra": "1.3.0"
+      },
+      {
+        "version": "2.2.1",
+        "sonar": "6.7.3 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14264",
+        "sonarScala": "6.1.0",
+        "sonarScalaExtra": "1.3.0"
+      },
+      {
+        "version": "2.2.0",
+        "sonar": "6.7.2 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14191",
+        "sonarScala": "6.1.0",
+        "sonarScalaExtra": "1.3.0"
+      },
+      {
+        "version": "2.1.0",
+        "sonar": "6.7.1 LTS",
+        "sonarDocs": "https://docs.sonarqube.org/display/SONARQUBE67/Documentation",
+        "sonarChangelog": "https://jira.sonarsource.com/jira/secure/ReleaseNote.jspa?projectId=10930&version=14137",
+        "sonarScala": "6.0.0",
+        "sonarScalaExtra": "1.3.0"
+      }
+    ]
+  }
 }
