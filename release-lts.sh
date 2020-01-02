4217008fd7000b4a55bfbde3f1e31aaf714011bd
Michael Wizner <mwz@users.noreply.github.com>
2020-01-02 18:22:55 +0000

    Add the "latest" tags to the release.

--- release-lts.sh
+++ release-lts.sh
@@ -2,9 +2,17 @@
 set -eu
 
 # Build
-docker build -t mwizner/sonarqube-scala-plugins:$VERSION lts
-docker build -t mwizner/sonarqube-scala-plugins:$VERSION-full lts/full
+docker build \
+  -t mwizner/sonarqube-scala-plugins:$VERSION \
+  -t mwizner/sonarqube-scala-plugins:latest-lts \
+  lts
+docker build \
+  -t mwizner/sonarqube-scala-plugins:$VERSION-full \
+  -t mwizner/sonarqube-scala-plugins:latest-lts-full \
+  lts/full
 
 # Push
 docker push mwizner/sonarqube-scala-plugins:$VERSION
+docker push mwizner/sonarqube-scala-plugins:latest-lts
 docker push mwizner/sonarqube-scala-plugins:$VERSION-full
+docker push mwizner/sonarqube-scala-plugins:latest-lts-full
