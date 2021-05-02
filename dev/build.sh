9a7dc6b0a943e77ac1a44b713760610901dd1c7e
Michael Wizner <mwz@users.noreply.github.com>
2021-05-02 17:44:52 +0100

    Update dev version

--- dev/build.sh
+++ dev/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -eu
 
-export SONAR_SCALA_VERSION=8.6.0+60-4ce01782-SNAPSHOT
+export SONAR_SCALA_VERSION=8.7.0+7-b082d533-SNAPSHOT
 cp ~/.ivy2/local/com.sonar-scala/sonar-scala_2.13/${SONAR_SCALA_VERSION}/jars/sonar-scala_2.13-assembly.jar .
 docker build -t mwizner/sonarqube-scala-plugins:dev --build-arg SONAR_SCALA_VERSION=${SONAR_SCALA_VERSION} .
